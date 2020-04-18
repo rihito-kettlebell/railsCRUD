9226198c9938bd137fd9eb427ed521909d0365c0
m.t-protain <rihitosakuraa155@gmail.com>
2020-04-18 18:59:03 +0900

    登録アクションを実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,6 +9,20 @@ class TasksController < ApplicationController
   def new
   end
 
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
   def edit
+  end
+
+  # ここから下はプライぺーとメソッド
+  private
+
+  # Strong Parametersという仕組み(不正データが登録・更新されることを防ぐ)を使用している
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
