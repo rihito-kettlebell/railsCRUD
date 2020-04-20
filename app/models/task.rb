3702af72da7779f3cfed7897b2afcb2f0d159303
m.t-protain <rihitosakuraa155@gmail.com>
2020-04-20 20:43:21 +0900

    コールバックの実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,15 @@
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   validate :validate_name_not_including_comma
 
   private
+
+  # before_validationのコールバックとして呼ぶ
+  def set_nameless_name
+    self.name = '名前なし' if name.blank?
+  end
 
   def validate_name_not_including_comma
     #nameがnilのときはこの検証を通る
