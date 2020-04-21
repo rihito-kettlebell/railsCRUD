778ea626383582ed26674768691078782cd5517c
m.t-protain <rihitosakuraa155@gmail.com>
2020-04-22 01:02:25 +0900

    ログイン処理の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,26 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    #送られてきたメルアドでユーザー検索
+    user = User.find_by(email: session_params[:email])
+
+    #authenticateメソッドは、認証のためのメソッド、
+    #引数で受け取ったパスワードをハッシュ化し、その結果がUserオブジェクト内部に保存されているdigestと一致するかどうかを調べる
+    if user&.authenticate(session_params[:password])
+      #セッションに格納
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'ログインしました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+
 end
