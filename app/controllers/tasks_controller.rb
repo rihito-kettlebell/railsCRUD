7f0901f42f321c67e31a6f9cf1d1c145cda2152e
m.t-protain <rihitosakuraa155@gmail.com>
2020-04-23 01:30:19 +0900

    フィルタでメソッド重複を避ける。DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  #指定したメソッドの処理を共通化できる
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     #Task.where(user_id: current_user.id)
     @tasks = current_user.tasks.order(created_at: :desc)
@@ -6,7 +8,6 @@ class TasksController < ApplicationController
 
   def show
     #idによって、DBからマッチするレコードを取得する
-    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -27,21 +28,20 @@ class TasksController < ApplicationController
 
   #受信したデータと一致するデータを検索し、編集画面へ表示する
   def edit
-    @task = current_user.tasks.find(params[:id])
   end
 
   #更新処理を行う
   def update
-    task = current_user.tasks.find(params[:id])
+    #
     #正常データのみを抽出し、DBを更新
-    task.update!(task_params)
+    @task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
   end
 
   #削除処理を行う
   def destroy
-    task = current_user.tasks.find(params[:id])
-    task.destroy
+    #
+    @task.destroy
     redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
 
@@ -51,5 +51,9 @@ class TasksController < ApplicationController
   # Strong Parametersという仕組み(不正データが登録・更新されることを防ぐ)を使用している
   def task_params
     params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
