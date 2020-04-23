43da9ec132d97047cf7eccfb32d628881244ef34
m.t-protain <rihitosakuraa155@gmail.com>
2020-04-24 01:37:27 +0900

    ユーザーAとそのタスクの作成

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,19 +1,23 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     before do
       # ユーザーAを作成しておく
+      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
       # 作成者がユーザーAであるタスクを作成しておく
+      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+    end
 
-      context 'ユーザーAがログインしているとき' do
-        before do
-          # ユーザーAでログインする
-        end
+    context 'ユーザーAがログインしているとき' do
+      before do
+        # ユーザーAでログインする
+      end
 
-        it 'ユーザーAが作成したタスクが表示される' do
-          # 作成済みのタスクの名称が画面上に表示されていることを確認
-        end
+      it 'ユーザーAが作成したタスクが表示される' do
+        # 作成済みのタスクの名称が画面上に表示されていることを確認
       end
     end
   end
