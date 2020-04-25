5c3b0c04831a547eb7dca341356506b6fb7f0b28
m.t-protain <rihitosakuraa155@gmail.com>
2020-04-25 20:49:57 +0900

    テスト失敗時の調査

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,6 @@
 class Task < ApplicationRecord
   before_validation :set_nameless_name
-  validates :name, presence: true
-  validates :name, length: { maximum: 30 }
+  validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_comma
 
   belongs_to :user
