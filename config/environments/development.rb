08ce201550952dc2c89f3d7300d65987b58b4151
m.t-protain <rihitosakuraa155@gmail.com>
2020-04-26 03:00:06 +0900

    ロガーの設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # application common logger
+  config.logger = Logger.new('log/development.log', 'daily')
+  config.custom_logger = Logger.new('log/custom.log', 'weekly')
 end
